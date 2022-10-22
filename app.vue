35c4eb6519201b58701e07c153240390d922f8b0
maximesflowers <maxime.ruppeiner@gmail.com>
2022-10-22 19:41:58 +0200

    Startseite neu

--- app.vue
+++ app.vue
@@ -4,6 +4,12 @@
 
 <template>
   <Head>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+
+
     <Title>Robo</Title>
   </Head>
 
--- components/sandbox/MaximeComp.vue
+++ components/sandbox/MaximeComp.vue
@@ -9,21 +9,27 @@
     <h1 class="text-black font-extrabold text-5xl col-start-2"> teach-a-bot </h1>
 </div> -->
 
-<h1 class="titeltext text-black font-extrabold text-5xl text-center pt-20"> teach-a-bot </h1>
+<h1 class="titeltext text-black font-extrabold text-6xl text-center pt-20 font-mono"> teach-a-bot </h1>
 
 <div class="sprechblase-hallo-robo relative"> 
-<img class="h-[100px]  ml-auto mr-48 mt-16" src="~/public/images/blase01_robotsayshello.svg" alt="sprechblase-hallo-robo">    
-<div class="sprechblase-text text-5xl absolute top-0 right-0 pr-72 pt-6"> hello </div>
+<img class="h-[100px]  ml-auto mr-48 mt-16" src="/images/blase01_robotsayshello.svg" alt="sprechblase-hallo-robo">    
+<div class="sprechblase-text text-5xl absolute top-0 right-0 pr-72 pt-6 font-sans"> hello </div>
 </div>
-<div class="Roboter-kopf w-60 h-48 bg-white ml-auto mr-auto mb-12">
+<div class="Roboter-kopf w-60 h-48 bg-white ml-auto mr-auto mb-8 mt-8">
 <img class="" src=" " alt="">
 </div>
 
-<div class="sprechblase-code-hallo relative"> 
-<div class="sprechblase-text text-5xl absolute bottom-0 left-96 pb-48 pl-48"> robo.log(-----------) </div>
-<img class="h-[200px] ml-auto mr-auto" src="~/public/images/blase01_isayhello.svg" alt="sprechblase-code-hallo">  
-<div class="anweisung-begrüssung text-2xl pb-7 pl-7 pt-7 w-96 ml-auto"> type "hello" to start >> </div>  
+<div class="sprechblase-code-hallo relative flex justify-center items-center"> 
+    <div class="sprechblase-text text-5xl absolute font-sans pb-10 flex"> 
+        <span> robo.log(' </span>  
+        <input type="text">
+        <span> ')</span> </div>
+    <img class="h-[200px]" src="/images/blase01_isayhello.svg" alt="sprechblase-code-hallo">  
 </div>
+<div class="anweisung-begrüssung text-2xl text-right font-mono mr-8"> type "hello" to start >> </div>  
 
 
 </template>
+
+
+
