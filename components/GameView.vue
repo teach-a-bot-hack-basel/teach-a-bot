004dd9ec449304d6b27b6b62bd1ab2c66545576a
julianvorraro <jvorraro@webentertainer.ch>
2022-10-22 19:32:21 +0200

    obstacles in GameView

--- components/GameView.vue
+++ components/GameView.vue
@@ -1,13 +1,17 @@
 <script lang="ts" setup>
-  import {Robot} from "~/classes/Robot"
+import {Robot} from "~/classes/Robot"
 
-  defineProps<{
-    robo: Robot,
-  }>()
+defineProps<{
+  robo: Robot,
+}>()
+
 </script>
 
 <template>
-  <ul>
-    <li v-for="obst of robo._level.obstacles">{{ obst }}</li>
-  </ul>
+  <div class="grid aspect-square bg-green-50" style="
+  grid-template-columns: repeat(24, minmax(0, 1fr));
+  grid-template-rows: repeat(24, minmax(0, 1fr));">
+    <div v-for="obst of robo._level.obstacles" class="bg-black"
+         :style="{gridColumnStart: obst, gridRowStart: 24}"/>
+  </div>
 </template>
--- pages/chapter/console/1.vue
+++ pages/chapter/console/1.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
 import GameView from "~/components/GameView.vue";
 import Controller from "~/components/Controller.vue"
+import {Robot} from "~/classes/Robot";
+import {Level} from "~/classes/Level";
+
+const level = new Level({obstacles: new Set([2, 4, 8])})
+const robo = new Robot({level})
+
 </script>
 <template>
   <div class="flex flex-col aspect-square">
-    <GameView class="flex-grow"/>
+    <GameView :robo="robo" class="flex-grow"/>
     <Controller class="fixed bottom-0"/>
   </div>
 </template>
