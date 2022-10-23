5247e65fd023dbbe01289caf937b74da6c7de80c
maximesflowers <maxime.ruppeiner@gmail.com>
2022-10-23 13:30:11 +0200

    New Start Pages

--- pages/tutorial/1.vue
+++ pages/tutorial/1.vue
@@ -1,46 +1,41 @@
-<script lang="ts" setup>
-</script>
 
 <template>
-
-  <div class=" bg-slate-300 p-6 w-full h-full">
-    <div class="bg-white w-full h-full flex-col p-16">
-      <div class="flex">
-        <div class="Roboter-ganz w-80 mt-16 ml-16 mr-64">
-          <img
-            class="w-80 mt-16 ml-16 mr-64"
-            src="/images/robo_ganz_schwebend.svg"
-            alt="Robo-ganz"
-          >
-        </div>
-
-        <div>
-          <div class="text-black font-extrabold text-3xl font-mono mb-24 text-center leading-loose">
-            How to be a robot &lt;Manual>
-          </div>
-          <div class="text-3xl font-sans text-center"> JavaScript is primarily
-            known as the language of most modern web browsers. The language has
-            continued to evolve and improve. JavaScript is a powerful, flexible,
-            and fast programming language now being used for increasingly
-            complex web development and beyond!
-
-            Since JavaScript remains at the core of web development, it’s often
-            the first language learned by self-taught coders eager to learn and
-            build. We’re excited for what you’ll be able to create with the
-            JavaScript foundation you gain here. JavaScript powers the dynamic
-            behavior on most websites, including this one.
-
-          </div>
+    <div class="p-4">
+      <h1 class="titeltext text-black font-extrabold text-6xl text-center pt-20 font-mono">
+        teach-a-bot </h1>
+  
+      <div class="sprechblase-hallo-robo relative">
+        <img
+          class="h-[100px]  ml-auto mr-48 mt-16"
+          src="/images/blase01_robotsayshello.svg"
+          alt="sprechblase-hallo-robo"
+        >
+        <div class="sprechblase-text text-5xl absolute top-0 right-0 pr-72 pt-6 font-sans">
+          hello
         </div>
       </div>
-
+  
+      <div class="Roboter-kopf w-60 h-48 ml-auto mr-auto mb-16 mt-8">
+        <img class="h-48" src="/images/robo_kopf.svg" alt="Robo-Kopf">
+      </div>
+  
+      <div class="sprechblase-code-hallo relative flex justify-center items-center">
+        <div class="sprechblase-text text-5xl absolute font-sans pb-10 flex">
+            hello
+        </div>
+        <img
+          class="h-[200px]"
+          src="/images/blase02_isaidhello.svg"
+          alt="sprechblase-code-hallo"
+        >
+      </div>
+  
       <RouterLink
-        to="/chapter/console/1"
-        class="block text-black font-extrabold text-2xl text-right font-mono mr-8 mt-8"
+        to="/tutorial/2"
+        class="block anweisung-begrüssung text-2xl text-right font-mono mr-8"
       >
-        Continue >>
+        continue >>
       </RouterLink>
-
     </div>
-  </div>
-</template>
+  </template>
+  
---
+++ pages/tutorial/3.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div class=" bg-slate-300 p-6 w-full h-full">
+      <div class="bg-white w-full h-full flex-col p-16">
+        <div class="flex">
+          <div class="Roboter-ganz w-80 mt-16 ml-16 mr-64">
+            <img
+              class="w-80 mt-16 ml-16 mr-64"
+              src="/images/robo_ganz_schwebend.svg"
+              alt="Robo-ganz"
+            >
+          </div>
+  
+          <div>
+            <div class="text-black font-extrabold text-3xl font-mono mb-24 text-center leading-loose">
+              readme.txt
+            </div>
+            <div class="text-3xl font-sans text-center"> The E-learning Game will start when you click the continue button.
+                Please open your browser-console bevor you start to play.
+                Open it with right-click on the page and click on the inspect button. 
+
+            <a href="https://appuals.com/open-browser-console/">
+            <button class="button-help text-black font-extrabold ml-auto mr-auto text-2xl font-mono mt-24 text-center"> 
+                &lt;Help me> </button>
+            </a>
+
+                        
+            </div>
+          </div>
+        </div>
+  
+        <RouterLink
+          to="/chapter/console/1"
+          class="block text-black font-extrabold text-2xl text-right font-mono mr-8 mt-8"
+        >
+          Continue >>
+        </RouterLink>
+  
+      </div>
+    </div>
+  </template>
