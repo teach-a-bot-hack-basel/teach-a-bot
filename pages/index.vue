92cdaee5cdaddac0fb86bee035cb6969433580f0
Simon Jaeger <simon@m2101k9g.home>
2022-10-23 12:01:51 +0200

    fix: start button

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,44 @@
 <script setup lang="ts">
-  // import {onMounted} from "#imports"
-  // import {Robot} from "~/classes/Robot"
-  // import {Level} from "~/classes/Level"
-  // import Counter from "~/components/Counter.vue"
-  // import GameView from "~/components/GameView.vue"
-
-  // const level = new Level({obstacles: new Set([2, 4, 8])})
-  // const robo = new Robot({level})
-
 </script>
 
 <template>
-<div class="p-4">
-   <h1 class="titeltext text-black font-extrabold text-6xl text-center pt-20 font-mono"> teach-a-bot </h1>
+  <div class="p-4">
+    <h1 class="titeltext text-black font-extrabold text-6xl text-center pt-20 font-mono">
+      teach-a-bot </h1>
 
-   <div class="sprechblase-hallo-robo relative"> 
-       <img class="h-[100px]  ml-auto mr-48 mt-16" src="/images/blase01_robotsayshello.svg" alt="sprechblase-hallo-robo">    
-       <div class="sprechblase-text text-5xl absolute top-0 right-0 pr-72 pt-6 font-sans"> hello </div>
-   </div>
-
-
-   <div class="Roboter-kopf w-60 h-48 ml-auto mr-auto mb-16 mt-8">
-       <img class="h-48" src="/images/robo_kopf.svg" alt="Robo-Kopf">
-   </div>
-
-   <div class="sprechblase-code-hallo relative flex justify-center items-center"> 
-       <div class="sprechblase-text text-5xl absolute font-sans pb-10 flex"> 
-           <span> robo.log(' </span>  
-           <input class="w-24 text-gray-300" type="text">
-           <span> ')</span> 
-       </div>
-       <img class="h-[200px]" src="/images/blase01_isayhello.svg" alt="sprechblase-code-hallo">  
-   </div>  
-
-   <RouterLink to="/tutorial/1" class="block anweisung-begrüssung text-2xl text-right font-mono mr-8">
-     type "hello" and start >> 
-    </RouterLink>
-</div>
-
-
-
-  <!-- <main>
-    <pre>{{ JSON.stringify(robo, null, 2) }}</pre>
-    <pre>obstacles: {{ [...level.obstacles].join(",") }}</pre>
-    <div class="w-96 h-32 bg-black relative">
-      <div
-        class="w-8 h-8 bg-gray-500 absolute"
-        :style="{left: (robo._x * 32)+'px', bottom: (robo._y * 32)+'px'}"
-      ></div>
+    <div class="sprechblase-hallo-robo relative">
+      <img
+        class="h-[100px]  ml-auto mr-48 mt-16"
+        src="/images/blase01_robotsayshello.svg"
+        alt="sprechblase-hallo-robo"
+      >
+      <div class="sprechblase-text text-5xl absolute top-0 right-0 pr-72 pt-6 font-sans">
+        hello
+      </div>
     </div>
-  </main> -->
+
+    <div class="Roboter-kopf w-60 h-48 ml-auto mr-auto mb-16 mt-8">
+      <img class="h-48" src="/images/robo_kopf.svg" alt="Robo-Kopf">
+    </div>
+
+    <div class="sprechblase-code-hallo relative flex justify-center items-center">
+      <div class="sprechblase-text text-5xl absolute font-sans pb-10 flex">
+        <span> robo.log(' </span>
+        <input class="w-24 text-gray-300" type="text">
+        <span> ')</span>
+      </div>
+      <img
+        class="h-[200px]"
+        src="/images/blase01_isayhello.svg"
+        alt="sprechblase-code-hallo"
+      >
+    </div>
+
+    <RouterLink
+      to="/tutorial/1"
+      class="block anweisung-begrüssung text-2xl text-right font-mono mr-8"
+    >
+      type "hello" and start >>
+    </RouterLink>
+  </div>
 </template>
--- pages/tutorial/1.vue
+++ pages/tutorial/1.vue
@@ -3,28 +3,44 @@
 
 <template>
 
-<div class=" bg-slate-300 p-6 w-full h-full"> 
-    <div class="bg-white w-full h-full flex-col p-16"> 
-        <div class="flex">
-            <div class="Roboter-ganz w-80 mt-16 ml-16 mr-64">
-            <img class = "w-80 mt-16 ml-16 mr-64" src="/images/robo_ganz_schwebend.svg" alt="Robo-ganz">
-            </div>
-
-            <div>
-                <div class="text-black font-extrabold text-3xl font-mono mb-24 text-center leading-loose"> How to be a robot &lt;Manual>
-                 </div>
-                 <div class="text-3xl font-sans text-center"> JavaScript is primarily known as the language of most modern web browsers. The language has continued to evolve and improve. JavaScript is a powerful, flexible, and fast programming language now being used for increasingly complex web development and beyond!
-
-Since JavaScript remains at the core of web development, it’s often the first language learned by self-taught coders eager to learn and build. We’re excited for what you’ll be able to create with the JavaScript foundation you gain here. JavaScript powers the dynamic behavior on most websites, including this one.
-                    
-                </div>
-             </div>
+  <div class=" bg-slate-300 p-6 w-full h-full">
+    <div class="bg-white w-full h-full flex-col p-16">
+      <div class="flex">
+        <div class="Roboter-ganz w-80 mt-16 ml-16 mr-64">
+          <img
+            class="w-80 mt-16 ml-16 mr-64"
+            src="/images/robo_ganz_schwebend.svg"
+            alt="Robo-ganz"
+          >
         </div>
-    
-        <RouterLink to="/chapter/console/1" class="block text-black font-extrabold text-2xl text-right font-mono mr-8 mt-8">
-            Continue >>
-        </RouterLink>
+
+        <div>
+          <div class="text-black font-extrabold text-3xl font-mono mb-24 text-center leading-loose">
+            How to be a robot &lt;Manual>
+          </div>
+          <div class="text-3xl font-sans text-center"> JavaScript is primarily
+            known as the language of most modern web browsers. The language has
+            continued to evolve and improve. JavaScript is a powerful, flexible,
+            and fast programming language now being used for increasingly
+            complex web development and beyond!
+
+            Since JavaScript remains at the core of web development, it’s often
+            the first language learned by self-taught coders eager to learn and
+            build. We’re excited for what you’ll be able to create with the
+            JavaScript foundation you gain here. JavaScript powers the dynamic
+            behavior on most websites, including this one.
+
+          </div>
+        </div>
+      </div>
+
+      <RouterLink
+        to="/chapter/console/1"
+        class="block text-black font-extrabold text-2xl text-right font-mono mr-8 mt-8"
+      >
+        Continue >>
+      </RouterLink>
 
     </div>
-</div>
+  </div>
 </template>
