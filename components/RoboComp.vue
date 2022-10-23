096740432e788eb66ed62787a320392b43e0a50d
Alexander Areesanan <gevoha27@pzg-22nbm01.chemie.unibas.ch>
2022-10-23 11:18:48 +0200

    Robo Komponente Erstellt

---
+++ components/RoboComp.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  // example counter component
+  let count = $ref(0)
+</script>
+
+<template>
+    <h2>Hello2
+
+    </h2>
+
+  <button class="p-4 w-full bg-red border" @click="count++">{{ count }}</button>
+  
+</template>
